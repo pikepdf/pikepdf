5b41e9ef732a641bbc5236b55cb35c25eb031104
James R. Barlow <jim@purplerock.ca>
2017-02-09 00:28:26 -0800

    OperandGrouper: collect operands and operators into lists
    Parsing is insane otherwise

--- pikepdf/__init__.py
+++ pikepdf/__init__.py
@@ -2,6 +2,24 @@ from . import qpdf
 import collections.abc
 
 
+class OperandGrouper(qpdf.StreamParser):
+    def __init__(self):
+        super().__init__()
+        self.groups = []
+        self.operands = []
+
+    def handle_object(self, obj):
+        if obj.type_code == qpdf.ObjectType.ot_operator:
+            self.groups.append((self.operands, obj))
+            self.operands = []
+        else:
+            self.operands.append(obj)
+
+    def handle_eof(self):
+        if self.operands:
+            raise EOFError("Unexpected end of stream")
+
+
 class PdfObject:
     @classmethod
     def specialize(cls, obj):
