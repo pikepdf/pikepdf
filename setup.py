4a78930e866cccb97bf161449f1599942fbcdb9b
James R. Barlow <jim@purplerock.ca>
2017-08-04 15:23:18 -0700

    Use xdist

--- setup.py
+++ setup.py
@@ -103,6 +103,6 @@ setup(
     cmdclass={'build_ext': BuildExt},
     zip_safe=False,
     setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    tests_require=['pytest', 'pytest-xdist'],
     packages=setuptools.find_packages()
 )
