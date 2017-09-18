9b2da8dfd60befdbc52b99eb02ec003d9c495c6c
James R. Barlow <jim@purplerock.ca>
2017-09-17 20:19:46 -0700

    Add make_unique definition to support C++11

--- src/pikepdf.h
+++ src/pikepdf.h
@@ -12,24 +12,24 @@
 #include <qpdf/QPDFObjectHandle.hh>
 #include <pybind11/pybind11.h>
 
+#if __cplusplus < 201402L  // If C++11
+
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+// Provide make_unique for C++11 (not array-capable)
+template<typename T, typename ...Args>
+std::unique_ptr<T> make_unique( Args&& ...args )
+{
+    return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
+}
+
+#endif // }}
+
 namespace pybind11 {
     PYBIND11_RUNTIME_EXCEPTION(attr_error, PyExc_AttributeError);
     PYBIND11_RUNTIME_EXCEPTION(notimpl_error, PyExc_NotImplementedError);
-};
-
-// Declare PointerHolder<T> as a smart pointer
-// https://pybind11.readthedocs.io/en/stable/advanced/smart_ptrs.html#custom-smart-pointers
-PYBIND11_DECLARE_HOLDER_TYPE(T, PointerHolder<T>);
-namespace pybind11 { namespace detail {
-    template <typename T>
-    struct holder_helper<PointerHolder<T>> {
-        static const T *get(const PointerHolder<T> &p) { return p.getPointer(); }
-    };
-}}
-
-namespace py = pybind11;
-
-QPDFObjectHandle objecthandle_encode(py::handle obj);otImplementedError);
 };
 
 // Declare PointerHolder<T> as a smart pointer
