e2c9d39dd30a60c5c54521d7d11773430cae1bd1
James R. Barlow <jim@purplerock.ca>
2017-11-02 22:56:26 -0700

    Add manual experiment that replaces a RGB image with grayscale

--- tests/test_image_access.py
+++ tests/test_image_access.py
@@ -2,21 +2,39 @@ import pytest
 import imghdr
 from io import BytesIO
 from PIL import Image
+import zlib
 
-from pikepdf import _qpdf as qpdf
+from pikepdf import Pdf, Object
 
 def test_jpeg(resources, outdir):
-    pdf = qpdf.Pdf.open(resources / 'congress.pdf')
+    pdf = Pdf.open(resources / 'congress.pdf')
 
     # If you are looking at this as example code, Im0 is not necessarily the
     # name of any image.
-    pdfimage = pdf.pages[0].Resources.XObject.Im0
-    raw_stream = pdf.pages[0].Resources.XObject.Im0.read_raw_stream()
+    pdfimage = pdf.pages[0].Resources.XObject['/Im0']
+    raw_bytes = pdfimage.read_raw_bytes()
     with pytest.raises(RuntimeError):
-        pdf.pages[0].Resources.XObject.Im0.read_stream()
+        pdfimage.read_bytes()
 
-    assert imghdr.what('', h=raw_stream) == 'jpeg'
+    assert imghdr.what('', h=raw_bytes) == 'jpeg'
 
-    im = Image.open(BytesIO(raw_stream))
+    im = Image.open(BytesIO(raw_bytes))
     assert im.size == (pdfimage.Width, pdfimage.Height)
     assert im.mode == 'RGB'
+
+
+def test_replace_jpeg(resources, outdir):
+    pdf = Pdf.open(resources / 'congress.pdf')
+
+    pdfimage = pdf.pages[0].Resources.XObject['/Im0']
+    raw_bytes = pdfimage.read_raw_bytes()
+
+    im = Image.open(BytesIO(raw_bytes))
+    grayscale = im.convert('L')
+
+    #newimage = Object.Stream(pdf, grayscale.tobytes())
+
+    pdfimage.write(zlib.compress(grayscale.tobytes()), Object.Name("/FlateDecode"), Object.Null())
+    pdfimage.ColorSpace = Object.Name('/DeviceGray')
+
+    pdf.save(outdir / 'congress_gray.pdf')
