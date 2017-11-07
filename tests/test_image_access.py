35c1afd5d0ad740ba96ec0571e7fe5d8f5e5b49b
James R. Barlow <jim@purplerock.ca>
2017-11-06 18:37:30 -0800

    Interestingly using test fixtures here causes a crash... why?

--- tests/test_image_access.py
+++ tests/test_image_access.py
@@ -4,38 +4,54 @@ from io import BytesIO
 from PIL import Image
 import zlib
 
-from pikepdf import Pdf, Object
+from pikepdf import Pdf, Object, PdfImage
 from pikepdf._qpdf import Name, Null
 
-def test_jpeg(resources, outdir):
-    pdf = Pdf.open(resources / 'congress.pdf')
 
-    # If you are looking at this as example code, Im0 is not necessarily the
-    # name of any image.
+@pytest.fixture
+def congress_im0(resources):
+    pdf = Pdf.open(resources / 'congress.pdf')
     pdfimage = pdf.pages[0].Resources.XObject['/Im0']
-    raw_bytes = pdfimage.read_raw_bytes()
+    return pdfimage
+
+
+def test_image_from_nonimage(resources):
+    pdf = Pdf.open(resources / 'congress.pdf')
+    resources = pdf.pages[0].Contents
+    with pytest.raises(TypeError):
+        PdfImage(resources)
+
+
+def test_image(congress_im0):
+    pdfimage = PdfImage(congress_im0)
+    pillowimage = pdfimage.topil()
+
+    assert pillowimage.mode == pdfimage.mode
+    assert pillowimage.size == pdfimage.size
+
+
+def test_lowlevel_jpeg(congress_im0, outdir):
+    raw_bytes = congress_im0.read_raw_bytes()
     with pytest.raises(RuntimeError):
-        pdfimage.read_bytes()
+        congress_im0.read_bytes()
 
     assert imghdr.what('', h=raw_bytes) == 'jpeg'
 
     im = Image.open(BytesIO(raw_bytes))
-    assert im.size == (pdfimage.Width, pdfimage.Height)
+    assert im.size == (congress_im0.Width, congress_im0.Height)
     assert im.mode == 'RGB'
 
 
-def test_replace_jpeg(resources, outdir):
-    pdf = Pdf.open(resources / 'congress.pdf')
-
-    pdfimage = pdf.pages[0].Resources.XObject['/Im0']
-    raw_bytes = pdfimage.read_raw_bytes()
+def test_lowlevel_replace_jpeg(congress_im0, outdir):
+    # This test will modify the PDF so needs its own image
+    raw_bytes = congress_im0.read_raw_bytes()
 
     im = Image.open(BytesIO(raw_bytes))
     grayscale = im.convert('L')
 
     #newimage = Object.Stream(pdf, grayscale.tobytes())
 
-    pdfimage.write(zlib.compress(grayscale.tobytes()), Name("/FlateDecode"), Null())
-    pdfimage.ColorSpace = Name('/DeviceGray')
+    congress_im0.write(zlib.compress(grayscale.tobytes()), Name("/FlateDecode"), Null())
+    congress_im0.ColorSpace = Name('/DeviceGray')
 
     pdf.save(outdir / 'congress_gray.pdf')
