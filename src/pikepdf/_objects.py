4ef38343bda780db2bc875ec9366d014a98c402c
James R. Barlow <jim@purplerock.ca>
2018-05-15 14:49:43 -0700

    Prohibit /Name with invalid utf-8 bytes

--- src/pikepdf/_objects.py
+++ src/pikepdf/_objects.py
@@ -78,6 +78,10 @@ class Name(metaclass=_ObjectMeta):
     object_type = ObjectType.name
 
     def __new__(cls, name):
+        # QPDF_Name::unparse ensures that names are always saved in a UTF-8
+        # compatible way, so we only need to guard the input.
+        if isinstance(name, bytes):
+            raise TypeError("Name should be str")
         return _qpdf._new_name(name)
 
 
