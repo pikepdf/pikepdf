7c21f9cc746a5d2530433e84e79d2a6444ce647d
James R. Barlow <jim@purplerock.ca>
2018-12-02 01:44:19 -0800

    metadata: synchronize docinfo

--- src/pikepdf/models/metadata.py
+++ src/pikepdf/models/metadata.py
@@ -5,14 +5,22 @@
 # Copyright (C) 2018, James R. Barlow (https://github.com/jbarlow83/)
 
 from functools import wraps
+from operator import itemgetter
+from datetime import datetime
 
 import libxmp
 
 from libxmp.utils import object_to_dict
 from libxmp import XMPMeta
 
-from .. import Stream
+from .. import __version__
+from .. import Stream, Name
 
+NAMESPACES = {
+    'dc': "http://purl.org/dc/elements/1.1/",
+    'pdf': "http://ns.adobe.com/pdf/1.3/",
+    'xmp': "http://ns.adobe.com/xap/1.0/",
+}
 
 def refresh(fn):
     @wraps(fn)
@@ -22,21 +30,24 @@ def refresh(fn):
         return fn(self, *args, **kwargs)
     return wrapper
 
-
 class PdfMetadata:
-    def __init__(self, pdf):
+    def __init__(self, pdf, pikepdf_mark=True, sync_docinfo=True):
         self.uris = {}
         self._pdf = pdf
         self._xmp = None
         self._records = {}
         self._flags = {}
+        self.mark = pikepdf_mark
+        self.sync_docinfo = sync_docinfo
 
     def _refresh(self):
         data = self._pdf.Root.Metadata.read_bytes()
         self._xmp = XMPMeta(xmp_str=data.decode('utf-8'))
         xmpdict = object_to_dict(self._xmp)
 
-        for uri, records in xmpdict.items():
+        # Sort to ensure all members of a compound object immediately follow
+        # the compound. Not sure if libxmp guarantees order.
+        for uri, records in sorted(xmpdict.items(), key=itemgetter(0)):
             for key, val, flags in records:
                 self.uris[key] = uri
                 self._flags[key] = flags
@@ -63,7 +74,11 @@ class PdfMetadata:
     def __enter__(self):
         return self._records
 
-    def _expected_type(self, key):
+    def _expected_type(self, key, val=None):
+        if key not in self._flags:
+            if isinstance(val, (list, set, dict, str)):
+                return type(val)
+            raise TypeError(val)
         if self._flags[key]['VALUE_IS_ARRAY']:
             if self._flags[key]['ARRAY_IS_ORDERED']:
                 return list
@@ -81,12 +96,35 @@ class PdfMetadata:
             return
         self._apply_changes()
 
+    def _update_docinfo(self):
+        MAPPING = {
+            (NAMESPACES['dc'], 'title'): Name.Title,
+            (NAMESPACES['dc'], 'subject'): Name.Subject,
+            (NAMESPACES['dc'], 'date'): Name.CreationDate,
+            (NAMESPACES['pdf'], 'Keywords'): Name.Keywords,
+            (NAMESPACES['pdf'], 'Producer'): Name.Producer,
+        }
+        for xmpparts, docinfo_name in MAPPING.items():
+            schema, element = xmpparts
+            value = self._xmp.get_property(schema, element)
+            self._pdf.docinfo[docinfo_name] = value
+
+        if 'dc:creator' in self._records:
+            creators = '; '.join(self._records['dc:creator'])
+            self._pdf.docinfo[Name.Authors] = creators
+
+    def _get_uri(self, key):
+        if key in self.uris:
+            return self.uris[key]
+        prefix = key.split(':', maxsplit=1)[0]
+        return self._xmp.get_namespace_for_prefix(prefix)
+
     def _apply_changes(self):
         for key, val in self._records.items():
-            if not isinstance(val, self._expected_type(key)):
+            if not isinstance(val, self._expected_type(key, val)):
                 raise TypeError(key)  # Kinda
 
-            uri = self.uris[key]
+            uri = self._get_uri(key)
             if isinstance(val, (list, set, dict)):
                 self._xmp.delete_property(uri, key)
                 array_options = self._property_options(self._flags[key])
@@ -95,8 +133,18 @@ class PdfMetadata:
             else:
                 self._xmp.set_property(uri, key, val)
 
+        if self.mark:
+            self._xmp.set_property_datetime(
+                NAMESPACES['xmp'], 'MetadataDate', datetime.now()
+            )
+            self._xmp.set_property(
+                NAMESPACES['pdf'], 'Producer', 'pikepdf ' + __version__
+            )
+
         data = self._xmp.serialize_to_unicode()
         self._pdf.Root.Metadata = Stream(self._pdf, data.encode('utf-8'))
+        if self.sync_docinfo:
+            self._update_docinfo()
         self._records = {}
 
     @refresh
--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -16,33 +16,33 @@ def graph(resources):
 def test_no_info(vera, outdir):
     assert vera.trailer.get('/Info') is None, 'need a test file with no /Info'
 
-    assert len(vera.metadata) == 0
+    assert len(vera.docinfo) == 0
     creator = 'pikepdf test suite'
-    vera.metadata['/Creator'] = creator
-    assert vera.metadata.is_indirect, "/Info must be an indirect object"
+    vera.docinfo['/Creator'] = creator
+    assert vera.docinfo.is_indirect, "/Info must be an indirect object"
     vera.save(outdir / 'out.pdf')
 
     new = Pdf.open(outdir / 'out.pdf')
-    assert new.metadata['/Creator'] == creator
+    assert new.docinfo['/Creator'] == creator
 
 
 def test_update_info(graph, outdir):
     new_title = '我敢打赌，你只是想看看这意味着什么'
-    graph.metadata['/Title'] = new_title
+    graph.docinfo['/Title'] = new_title
     graph.save(outdir / 'out.pdf')
 
     new = Pdf.open(outdir / 'out.pdf')
-    assert new.metadata['/Title'] == new_title
-    assert graph.metadata['/Author'] == new.metadata['/Author']
+    assert new.docinfo['/Title'] == new_title
+    assert graph.docinfo['/Author'] == new.docinfo['/Author']
 
     with pytest.raises(ValueError):
-        new.metadata = Dictionary({'/Keywords': 'bob'})
+        new.docinfo = Dictionary({'/Keywords': 'bob'})
 
-    new.metadata = graph.make_indirect(Dictionary({'/Keywords': 'bob'}))
-    assert new.metadata.is_indirect, "/Info must be an indirect object"
+    new.docinfo = graph.make_indirect(Dictionary({'/Keywords': 'bob'}))
+    assert new.docinfo.is_indirect, "/Info must be an indirect object"
 
 
 def test_copy_info(vera, graph, outdir):
-    vera.metadata = vera.copy_foreign(graph.metadata)
-    assert vera.metadata.is_indirect, "/Info must be an indirect object"
+    vera.docinfo = vera.copy_foreign(graph.docinfo)
+    assert vera.docinfo.is_indirect, "/Info must be an indirect object"
     vera.save(outdir / 'out.pdf')
