397ec48c7dc087a97ce49dc35bdee65a40daae58
James R. Barlow <jim@purplerock.ca>
2018-12-17 22:28:14 -0800

    metadata: expand list of standard namespaces

--- src/pikepdf/models/metadata.py
+++ src/pikepdf/models/metadata.py
@@ -27,18 +27,27 @@ XMP_NS_DC = "http://purl.org/dc/elements/1.1/"
 XMP_NS_PDF = "http://ns.adobe.com/pdf/1.3/"
 XMP_NS_PDFA_ID = "http://www.aiim.org/pdfa/ns/id/"
 XMP_NS_PDFX_ID = "http://www.npes.org/pdfx/ns/id/"
+XMP_NS_PHOTOSHOP = "http://ns.adobe.com/photoshop/1.0/"
+XMP_NS_PRISM2 = "http://prismstandard.org/namespaces/basic/2.0/"
+XMP_NS_PRISM3 = "http://prismstandard.org/namespaces/basic/3.0/"
 XMP_NS_RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
 XMP_NS_XMP = "http://ns.adobe.com/xap/1.0/"
 XMP_NS_XMP_MM = "http://ns.adobe.com/xap/1.0/mm/"
+XMP_NS_XMP_RIGHTS = "http://ns.adobe.com/xap/1.0/rights/"
 
 DEFAULT_NAMESPACES = [
     ('adobe:ns:meta/', 'x'),
     (XMP_NS_DC, 'dc'),
     (XMP_NS_PDF, 'pdf'),
     (XMP_NS_PDFA_ID, 'pdfaid'),
+    (XMP_NS_PDFX_ID, 'pdfxid'),
+    (XMP_NS_PHOTOSHOP, 'photoshop'),
+    (XMP_NS_PRISM2, 'prism2'),
+    (XMP_NS_PRISM3, 'prism3'),
     (XMP_NS_RDF, 'rdf'),
     (XMP_NS_XMP, 'xmp'),
-    (XMP_NS_XMP_MM, 'xapMM'),
+    (XMP_NS_XMP_MM, 'xmpMM'),
+    (XMP_NS_XMP_RIGHTS, 'xmpRights'),
 ]
 
 for _uri, _prefix in DEFAULT_NAMESPACES:
