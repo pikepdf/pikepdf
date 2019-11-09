fd2a1aa4b7b5541c93aa283b3a8ee06250e88756
James R. Barlow <jim@purplerock.ca>
2019-11-09 15:42:33 -0800

    Use Azure pipelines for deployment

--- azure-pipelines/build.bash
+++ azure-pipelines/build.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
 python -m pip install --upgrade pip
-pip install cibuildwheel==$CIBUILDWHEEL_VERSION
-cibuildwheel --output-dir wheelhouse .
+pip install cibuildwheel==$CIBUILDWHEEL_VERSION || \
+    pip install git+git://github.com/joerick/cibuildwheel.git@$CIBUILDWHEEL_VERSION
+cibuildwheel --output-dir dist .
