81f27ad3a1d414068311ce7f9edc11552c79494b
James R. Barlow <jim@purplerock.ca>
2019-11-11 22:29:06 -0800

    azure: many fixes, support linux 32 and 64

--- azure-pipelines/build.bash
+++ azure-pipelines/build.bash
@@ -3,4 +3,4 @@ set -ex
 python -m pip install --upgrade pip
 pip install cibuildwheel==$CIBUILDWHEEL_VERSION || \
     pip install git+git://github.com/joerick/cibuildwheel.git@$CIBUILDWHEEL_VERSION
-cibuildwheel --output-dir dist .
+cibuildwheel --output-dir wheelhouse .
