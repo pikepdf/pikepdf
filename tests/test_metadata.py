f5b103368fe00f9650cc7b6361808c3515db8ed4
James R. Barlow <jim@purplerock.ca>
2020-01-06 03:30:03 -0800

    Adjust skip conditions on previous test

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -2,6 +2,7 @@ import re
 from datetime import datetime, timedelta, timezone
 import os
 from pathlib import Path
+import platform
 import sys
 
 import xml.etree.ElementTree as ET
@@ -413,8 +414,8 @@ def test_no_x_xmpmeta(trivial):
     ],
 )
 def test_degenerate_xml(trivial, xml):
-    if xml == b'' and sys.version_info[0:2] <= (3, 5):
-        pytest.skip(msg="fails on macOS/Python 3.5; unknown cause")
+    if xml == b'' and platform.system() == 'Darwin':
+        pytest.skip(msg="fails on macOS/Python 3.5 and 3.8; unknown cause")
     trivial.Root.Metadata = trivial.make_stream(xml)
     with trivial.open_metadata() as xmp:
         xmp['pdfaid:part'] = '2'
