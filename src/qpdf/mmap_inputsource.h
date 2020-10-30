d84a020cb7659cedf3324f6a045e9000831bb763
James R. Barlow <james@purplerock.ca>
2020-10-30 01:46:52 -0700

    Remove code copied from QPDF and use QPDF instead
    Now that QPDF has accepted our performance patch we can do this.

--- src/qpdf/mmap_inputsource.h
+++ src/qpdf/mmap_inputsource.h
@@ -28,13 +28,10 @@
 #include "pikepdf.h"
 #include "utils.h"
 
-// We could almost subclass BufferInputSource, and we even blind copy much of its
-// code. The current reason to not do this is the performance improvement in
-// findAndSkipNextEOL(), which is 8x faster on some files than QPDF. We cannot
-// partially subclass or keep a BufferInputSource due to the need to manage
-// BufferInputSource::Members::cur_offset and InputSource::last_offset, the
-// former being a private variable. If QPDF accepts our changes to findAndSkipNextEOL
-// this could subclass BufferInputSource.
+// We could almost subclass BufferInputSource here, except that it expects Buffer
+// as an initialization parameter, we don't know what the buffer location is until
+// the mmap is set up. Instead, this class is an InputSource that has a
+// BufferInputSource.
 
 // GIL usage:
 // When opening the PDF, we release the GIL before calling processInputSource
@@ -46,7 +43,7 @@ class MmapInputSource : public InputSource
 {
 public:
     MmapInputSource(py::object stream, const std::string& description, bool close_stream) :
-            InputSource(), stream(stream), description(description), close_stream(close_stream), offset(0)
+            InputSource(), stream(stream), close_stream(close_stream)
     {
         py::gil_scoped_acquire acquire;
         py::int_ fileno = stream.attr("fileno")();
@@ -63,6 +60,16 @@ public:
         // Use a unique_ptr here so we can control the timing of our buffer_info's
         // deconstruction.
         this->buffer_info = std::make_unique<py::buffer_info>(view.request(false));
+
+        auto qpdf_buffer = std::make_unique<Buffer>(
+            static_cast<unsigned char*>(this->buffer_info->ptr),
+            this->buffer_info->size
+        );
+        this->bis = std::make_unique<BufferInputSource>(
+            description,
+            qpdf_buffer.release(),
+            false  // own_memory=false
+        );
     }
     virtual ~MmapInputSource()
     {
@@ -72,6 +79,7 @@ public:
             // buffer_info.reset() will trigger PyBuffer_Release(), which we must
             // do before we can close the memory mapping, since we exported a pointer
             // from it.
+            this->bis.reset();
             this->buffer_info.reset();
             if (!this->mmap.is_none()) {
                 this->mmap.attr("close")();
@@ -94,130 +102,43 @@ public:
 
     std::string const& getName() const override
     {
-        return this->description;
+        return this->bis->getName();
     }
 
     qpdf_offset_t tell() override
     {
-        return this->offset;
+        return this->bis->tell();
     }
 
     void seek(qpdf_offset_t offset, int whence) override
     {
-        switch(whence)
-        {
-            case SEEK_SET:
-                this->offset = offset;
-                break;
-            case SEEK_END:
-                this->offset = this->buffer_info->size + offset;
-                break;
-            case SEEK_CUR:
-                this->offset += offset;
-                break;
-            default:
-	            throw std::logic_error(
-	                "INTERNAL ERROR: invalid argument to MmapInputSource::seek"
-                );
-	            break;
-        }
-        if (this->offset < 0)
-        {
-            throw std::runtime_error(
-                this->description + ": seek before beginning of buffer");
-        }
+        this->bis->seek(offset, whence);
     }
 
     void rewind() override
     {
-        this->seek(0, SEEK_SET);
+        this->bis->rewind();
     }
 
     size_t read(char* buffer, size_t length) override
     {
-        if (this->offset < 0)
-        {
-            throw std::logic_error("INTERNAL ERROR: MmapInputSource offset < 0");
-        }
-        qpdf_offset_t end_pos = this->buffer_info->size;
-        if (this->offset >= end_pos)
-        {
-            this->last_offset = end_pos;
-            return 0;
-        }
-
-        this->last_offset = this->offset;
-        size_t len = std::min(QIntC::to_size(end_pos - this->offset), length);
-        const char *src = static_cast<const char*>(this->buffer_info->ptr) + this->offset;
-        {
-            // We can't tell if we released the GIL (initialization) or are
-            // holding it now, so check before release.
-            if (PyGILState_Check() == 1) {
-                py::gil_scoped_release gil;
-                memcpy(buffer, src, len);
-            } else {
-                memcpy(buffer, src, len);
-            }
-        }
-        this->offset += QIntC::to_offset(len);
-        return len;
+        return this->bis->read(buffer, length);
     }
 
     void unreadCh(char ch) override
     {
-        if (this->offset > 0) {
-            --this->offset;
-        }
+        this->bis->unreadCh(ch);
     }
 
     qpdf_offset_t findAndSkipNextEOL() override
     {
-        if (this->offset < 0)
-        {
-            throw std::logic_error("INTERNAL ERROR: MmapInputSource offset < 0");
-        }
-        qpdf_offset_t end_pos = this->buffer_info->size;
-        if (this->offset >= end_pos)
-        {
-            this->last_offset = end_pos;
-            this->offset = end_pos;
-            return end_pos;
-        }
-        qpdf_offset_t result = 0;
-        unsigned char const* buffer = static_cast<unsigned char const*>(this->buffer_info->ptr);
-
-        unsigned char const* end = buffer + end_pos;
-        unsigned char const* p = buffer + this->offset;
-
-        while (p < end) {
-            if (*p == '\r' || *p == '\n')
-                break;
-            ++p;
-        }
-        if (p != end) {
-            result = p - buffer;
-            this->offset = result + 1;
-            ++p;
-            while ((this->offset < end_pos) &&
-                ((*p == '\r') || (*p == '\n')))
-            {
-                ++p;
-                ++this->offset;
-            }
-        }
-        else
-        {
-            this->offset = end_pos;
-            result = end_pos;
-        }
-        return result;
+        return this->bis->findAndSkipNextEOL();
     }
 
 private:
     py::object stream;
-    std::string description;
     bool close_stream;
     py::object mmap;
     std::unique_ptr<py::buffer_info> buffer_info;
-    qpdf_offset_t offset;
+    std::unique_ptr<BufferInputSource> bis;
 };
