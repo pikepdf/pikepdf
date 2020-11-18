pikepdf/pikepdf

commit 6bb374885605010e6c4417e9584da9197fc5a773

Add QPDF error translator to improve copy_foreign errors

src/qpdf/pikepdf.cpp
@@ -12,6 +12,8 @@
 #include <cstring>
 #include <cstdio>
 #include <regex>
+#include <vector>
+#include <utility>
 
 #include "pikepdf.h"
 
@@ -41,6 +43,25 @@ public:
 private:
     int stored;
 };
+
+std::string translate_qpdf_error(std::string msg)
+{
+    using match_replace = std::pair<std::regex, std::string>;
+    const static std::vector<match_replace> replacements = {
+        match_replace{"QPDF::copyForeign(?:Object)?", "pikepdf.copy_foreign"},
+        match_replace{"QPDFObjectHandle", "pikepdf.Object"},
+        match_replace{"QPDF", "pikepdf.Pdf"},
+    };
+    for (auto mr : replacements) {
+        msg = std::regex_replace(msg, mr.first, mr.second);
+    }
+    return msg;
+}
+
+std::string translate_qpdf_error(const std::exception& e)
+{
+    return translate_qpdf_error(std::string(e.what()));
+}
 
 
 PYBIND11_MODULE(_qpdf, m) {
@@ -77,6 +98,12 @@ PYBIND11_MODULE(_qpdf, m) {
                 fclose(file);
         },
         "Used to test that C++ system error -> Python exception propagation works."
+    );
+
+    m.def("_translate_qpdf",
+        [](std::string s) {
+            return translate_qpdf_error(s);
+        }
     );
 
     m.def("set_decimal_precision",
@@ -120,11 +147,7 @@ PYBIND11_MODULE(_qpdf, m) {
                 exc_main(e.what());
             }
         } catch (const std::logic_error &e) {
-            std::string msg(e.what());
-            if (msg.find("copyForeign")) {
-                msg = std::regex_replace(msg, std::regex("QPDF::copyForeign(?:Object)?"), "Pdf.copy_foreign()");
-                exc_foreign(msg.c_str());
-            }
+            exc_foreign(translate_qpdf_error(e).c_str());
         }
     });
 

commit ea43c724ff51f11370ef3ca990a8f2f2a9129281

Fix QPDF error translator turning everything into ForeignObjectError
Commit 6bb37488 inadvertently turned all std::logic_error from
QPDF into ForeignObjectError which was not correct. This
restores previous behavior rather than making everything a PdfError,
which we should do selectively.

src/qpdf/pikepdf.cpp
@@ -44,25 +44,38 @@ private:
     int stored;
 };
 
-std::string translate_qpdf_error(std::string msg)
+enum pikepdf_error_type {
+    error_type_pdferror,
+    error_type_foreign,
+    error_type_cpp,
+};
+
+auto translate_qpdf_error(std::string msg)
 {
     using match_replace = std::pair<std::regex, std::string>;
+    pikepdf_error_type errtype;
     const static std::vector<match_replace> replacements = {
         match_replace{"QPDF::copyForeign(?:Object)?", "pikepdf.copy_foreign"},
         match_replace{"QPDFObjectHandle", "pikepdf.Object"},
         match_replace{"QPDF", "pikepdf.Pdf"},
     };
+
     for (auto mr : replacements) {
         msg = std::regex_replace(msg, mr.first, mr.second);
     }
-    return msg;
+    if (std::regex_search(msg, std::regex("pikepdf.copy_foreign")))
+        errtype = error_type_foreign;
+    else if (std::regex_search(msg, std::regex("pikepdf.")))
+        errtype = error_type_pdferror;
+    else
+        errtype = error_type_cpp;
+    return std::pair<std::string, pikepdf_error_type>(msg, errtype);
 }
 
-std::string translate_qpdf_error(const std::exception& e)
+auto translate_qpdf_error(const std::exception& e)
 {
     return translate_qpdf_error(std::string(e.what()));
 }
-
 
 PYBIND11_MODULE(_qpdf, m) {
     //py::options options;
@@ -102,7 +115,7 @@ PYBIND11_MODULE(_qpdf, m) {
 
     m.def("_translate_qpdf",
         [](std::string s) {
-            return translate_qpdf_error(s);
+            return translate_qpdf_error(s).first;
         }
     );
 
@@ -147,7 +160,13 @@ PYBIND11_MODULE(_qpdf, m) {
                 exc_main(e.what());
             }
         } catch (const std::logic_error &e) {
-            exc_foreign(translate_qpdf_error(e).c_str());
+            auto trans = translate_qpdf_error(e);
+            if (trans.second == error_type_foreign)
+                exc_foreign(trans.first.c_str());
+            else if (trans.second == error_type_pdferror)
+                exc_main(trans.first.c_str());
+            else
+                std::rethrow_exception(p);
         }
     });
 

tests/test_errors.py
@@ -1,0 +1,15 @@
+import pytest
+
+from pikepdf import Pdf
+
+
+@pytest.fixture
+def vera(resources):
+    # A file that is not linearized
+    return Pdf.open(resources / 'veraPDF test suite 6-2-10-t02-pass-a.pdf')
+
+
+def test_foreign_linearization(vera):
+    assert not vera.is_linearized
+    with pytest.raises(RuntimeError, match="not linearized"):
+        vera.check_linearization()