6b1a5cfc1881f7dd65b9597fda3799755dc9e4c5
James R. Barlow <james@purplerock.ca>
2020-11-23 16:35:24 -0800

    outlines: add missing copyright

--- src/pikepdf/models/outlines.py
+++ src/pikepdf/models/outlines.py
@@ -1,3 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (C) 2020, Matthias Erll
+# Copyright (C) 2020, James R. Barlow (https://github.com/jbarlow83/)
+
+
 from enum import Enum
 from itertools import chain
 from typing import Iterable, List, Optional, Set, Tuple, Union, cast
