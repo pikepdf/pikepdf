ce3e92a94dd65ddeb63f1ebef7c5538bd0c77db8
James R. Barlow <james@purplerock.ca>
2021-03-06 00:56:31 -0800

    azure: Remove remaining files

--- azure-pipelines/build.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-python -m pip install --upgrade pip
-pip install cibuildwheel==$CIBUILDWHEEL_VERSION || \
-    pip install git+git://github.com/joerick/cibuildwheel.git@$CIBUILDWHEEL_VERSION
-cibuildwheel --output-dir wheelhouse .
--- azure-pipelines/win-download-qpdf.ps1
+++
@@ -1,5 +0,0 @@
-Invoke-WebRequest -Uri $env:QPDF_WINDOWS -OutFile "qpdf-release.zip"
-7z x "qpdf-release.zip" -oc:\
-$qpdfdir = Get-ChildItem c:\qpdf-*
-Move-Item -Path $qpdfdir -Destination c:\qpdf
-cp c:\qpdf\bin\*.dll src\pikepdf
