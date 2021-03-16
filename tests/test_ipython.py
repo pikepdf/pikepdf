8645675cd194321e0ce8fd27b57214eeebcf7e02
James R. Barlow <james@purplerock.ca>
2021-03-15 18:06:09 -0700

    tests: improve coverage of mudraw behavior

--- tests/test_ipython.py
+++ tests/test_ipython.py
@@ -2,7 +2,12 @@
 Test IPython/Jupyter display hooks
 """
 
+import shutil
+import subprocess
+from io import BytesIO
+
 import pytest
+from PIL import Image
 
 import pikepdf
 
@@ -12,10 +17,52 @@ def pal(resources):
     return pikepdf.open(resources / 'pal-1bit-trivial.pdf')
 
 
-def test_display_page(pal):
-    page0 = pal.pages[0]
-    mimebundle = page0._repr_mimebundle_(include=None, exclude=None)
+def test_display_raw_page(pal):
+    raw_page0 = pal.pages[0]
+    mimebundle = raw_page0._repr_mimebundle_(
+        include=['application/pdf'], exclude=['application/malware']
+    )
     assert 'application/pdf' in mimebundle
+
+
+def test_display_rich_page(pal):
+    page0 = pikepdf.Page(pal.pages[0])
+    mimebundle = page0._repr_mimebundle_(
+        include=['application/pdf'], exclude=['application/malware']
+    )
+    assert 'application/pdf' in mimebundle
+
+
+def test_draw_page(pal, monkeypatch):
+    # Test page drawing error handling independent of whether mudraw is installed
+
+    page0 = pikepdf.Page(pal.pages[0])
+
+    def raise_filenotfound(prog_args, *args, **kwargs):
+        raise FileNotFoundError(prog_args[0])
+
+    monkeypatch.setattr(pikepdf._methods, 'run', raise_filenotfound)
+    mimebundle = page0._repr_mimebundle_(
+        include=['image/png'], exclude=['application/pdf']
+    )
+    assert (
+        'image/png' not in mimebundle
+    ), "Generated image/png when mudraw() was rigged to fail"
+
+    def return_simple_png(prog_args, *args, **kwargs):
+        im = Image.new('1', (1, 1))
+        bio = BytesIO()
+        im.save(bio, format='PNG')
+        bio.seek(0)
+        return subprocess.CompletedProcess(prog_args, 0, stdout=bio.read(), stderr=b'')
+
+    monkeypatch.setattr(pikepdf._methods, 'run', return_simple_png)
+    mimebundle = page0._repr_mimebundle_(
+        include=['image/png'], exclude=['application/pdf']
+    )
+    assert (
+        'image/png' in mimebundle
+    ), "Did not generate image/png when mudraw() was rigged to succeed"
 
 
 def test_display_image(pal):
@@ -37,3 +84,4 @@ def test_display_pdf(pal):
 def test_object_key_completion(pal):
     page0 = pal.pages[0]
     assert '/Type' in page0._ipython_key_completions_()
+    assert page0.MediaBox._ipython_key_completions_() is None
