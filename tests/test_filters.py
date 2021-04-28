36109a858ca80ef9279081aa4161c0bc6a25992c
James R. Barlow <james@purplerock.ca>
2021-04-28 00:42:55 -0700

    test_filters: cleanup imports

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,53 +1,49 @@
-import shutil
-import sys
-from subprocess import PIPE, run
-
 import pytest
 
-import pikepdf
+from pikepdf import Page, Pdf, PdfError, Token, TokenFilter, TokenType
 
 
 @pytest.fixture
 def pal(resources):
-    return pikepdf.open(resources / 'pal-1bit-rgb.pdf')
+    return Pdf.open(resources / 'pal-1bit-rgb.pdf')
 
 
-class FilterThru(pikepdf.TokenFilter):
+class FilterThru(TokenFilter):
     def handle_token(self, token):
         return token
 
 
-class FilterDrop(pikepdf.TokenFilter):
+class FilterDrop(TokenFilter):
     def handle_token(self, token):
         return None
 
 
-class FilterNumbers(pikepdf.TokenFilter):
+class FilterNumbers(TokenFilter):
     def __init__(self):
         super().__init__()
 
     def handle_token(self, token):
-        if token.type_ in (pikepdf.TokenType.real, pikepdf.TokenType.integer):
-            return [token, pikepdf.Token(pikepdf.TokenType.space, b" ")]
+        if token.type_ in (TokenType.real, TokenType.integer):
+            return [token, Token(TokenType.space, b" ")]
 
 
-class FilterCollectNames(pikepdf.TokenFilter):
+class FilterCollectNames(TokenFilter):
     def __init__(self):
         super().__init__()
         self.names = []
         self.rawnames = []
 
     def handle_token(self, token):
-        if token.type_ == pikepdf.TokenType.name:
+        if token.type_ == TokenType.name:
             self.names.append(token.value)
             self.rawnames.append(token.raw_value)
         return None
 
 
 def test_token_eq_token():
-    token_42 = pikepdf.Token(pikepdf.TokenType.integer, b'42')
-    assert pikepdf.Token(pikepdf.TokenType.space, b' ') != token_42
-    assert pikepdf.Token(pikepdf.TokenType.integer, b'42') == token_42
+    token_42 = Token(TokenType.integer, b'42')
+    assert Token(TokenType.space, b' ') != token_42
+    assert Token(TokenType.integer, b'42') == token_42
     assert token_42 != 42
     assert repr(token_42) == "pikepdf.Token(TokenType.integer, b'42')"
 
@@ -61,14 +57,14 @@ def test_token_eq_token():
     ],
 )
 def test_filter_thru(pal, filter, expected):
-    page = pikepdf.Page(pal.pages[0])
+    page = Page(pal.pages[0])
     page.add_content_token_filter(filter())
     after = page.obj.Contents.read_bytes()
     assert after == expected
 
 
 def test_filter_names(pal):
-    page = pikepdf.Page(pal.pages[0])
+    page = Page(pal.pages[0])
     filter = FilterCollectNames()
     result = page.get_filtered_contents(filter)
     assert result == b''
@@ -77,24 +73,24 @@ def test_filter_names(pal):
     assert after != b''
 
 
-class FilterInvalid(pikepdf.TokenFilter):
+class FilterInvalid(TokenFilter):
     def handle_token(self, token):
         return 42
 
 
 def test_invalid_handle_token(pal):
-    page = pikepdf.Page(pal.pages[0])
-    with pytest.raises((TypeError, pikepdf.PdfError)):
+    page = Page(pal.pages[0])
+    with pytest.raises((TypeError, PdfError)):
         page.get_filtered_contents(FilterInvalid())
 
 
 def test_invalid_tokenfilter(pal):
-    page = pikepdf.Page(pal.pages[0])
+    page = Page(pal.pages[0])
     with pytest.raises(TypeError):
         page.get_filtered_contents(list())
 
 
 def test_tokenfilter_is_abstract(pal):
-    page = pikepdf.Page(pal.pages[0])
-    with pytest.raises((RuntimeError, pikepdf.PdfError)):
-        page.get_filtered_contents(pikepdf.TokenFilter())
+    page = Page(pal.pages[0])
+    with pytest.raises((RuntimeError, PdfError)):
+        page.get_filtered_contents(TokenFilter())
