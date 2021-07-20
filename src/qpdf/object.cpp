c5445c09d6d1a5eeaabeedaa1aea5d7f4f204672
James R. Barlow <james@purplerock.ca>
2021-07-20 00:32:33 -0700

    Fix pybind11 2.7.0 breakage
    This release seemingly introduced some changes around utf-8 surrogate handling
    that violated some of our test assumptions. Adjust for this.
    
    (cherry picked from commit e537b81f15773cc8028ba5781cb965050307dae7)

--- src/qpdf/object.cpp
+++ src/qpdf/object.cpp
@@ -326,6 +326,20 @@ void init_object(py::module_ &m)
             py::is_operator())
         .def(
             "__eq__",
+            [](QPDFObjectHandle &self, py::bytes other) {
+                std::string bytes_other = other.cast<std::string>();
+                switch (self.getTypeCode()) {
+                case QPDFObject::object_type_e::ot_string:
+                    return self.getStringValue() == bytes_other;
+                case QPDFObject::object_type_e::ot_name:
+                    return self.getName() == bytes_other;
+                default:
+                    return false;
+                }
+            },
+            py::is_operator())
+        .def(
+            "__eq__",
             [](QPDFObjectHandle &self, py::object other) -> py::object {
                 QPDFObjectHandle q_other;
                 try {
