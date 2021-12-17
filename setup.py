b2f51ab7a96387ea16d17e88baa5713daf3ab7e3
James R. Barlow <james@purplerock.ca>
2021-12-17 00:11:03 -0800

    setup: use rpath linking for QPDF_SOURCE_TREE

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import sys
 from glob import glob
+from itertools import chain
 from os import environ
-from os.path import join
+from os.path import exists, join
 from typing import List, cast
 
 from pybind11.setup_helpers import ParallelCompile, Pybind11Extension, build_ext
@@ -9,7 +10,7 @@ from setuptools import Extension, setup
 
 extra_includes = []
 extra_library_dirs = []
-qpdf_source_tree = environ.get('QPDF_SOURCE_TREE', None)
+qpdf_source_tree = environ.get('QPDF_SOURCE_TREE', '')
 if qpdf_source_tree:
     # Point this to qpdf source tree built with shared libaries
     extra_includes.append(join(qpdf_source_tree, 'include'))
@@ -25,7 +26,14 @@ except ImportError:
     __version__ = '0.0.1'
 
 
-ext_modules = [
+for extra_path in chain([qpdf_source_tree], extra_includes, extra_library_dirs):
+    if extra_path and not exists(extra_path):
+        raise FileNotFoundError(extra_path)
+
+# Use cast because mypy has trouble seeing Pybind11Extension is a subclass of
+# Extension.
+extmodule: Extension = cast(
+    Extension,
     Pybind11Extension(
         'pikepdf._qpdf',
         sorted(glob('src/qpdf/*.cpp')),
@@ -37,27 +45,32 @@ ext_modules = [
         library_dirs=[*extra_library_dirs],
         libraries=['qpdf'],
         cxx_std=17,
-    )
-]
+    ),
+)
 
 if sys.platform == 'cygwin':
     # On cygwin, use gnu++17 instead of c++17
-    eca = ext_modules[0].extra_compile_args
+    eca = extmodule.extra_compile_args
     eca[eca.index('-std=c++17')] = '-std=gnu++17'
 
 # Debug build
-# ext_modules[0].extra_compile_args.append('-g3')
+# module[0].extra_compile_args.append('-g3')
+
+if qpdf_source_tree:
+    for lib in extra_library_dirs:
+        extmodule.extra_link_args.append(  # type: ignore
+            f'-Wl,-rpath={lib},--enable-new-dtags'
+        )
 
 
 if __name__ == '__main__':
-    if ParallelCompile:
-        ParallelCompile().install()
-    setup(
-        setup_requires=[  # can be removed whenever we can drop pip 9 support
-            'setuptools_scm',  # so that version will work
-            'setuptools_scm_git_archive',  # enable version from github tarballs
-        ],
-        ext_modules=cast(List[Extension], ext_modules),
-        use_scm_version=True,
-        cmdclass={"build_ext": build_ext},
-    )
+    with ParallelCompile("PIKEPDF_NUM_BUILD_JOBS"):  # optional envvar
+        setup(
+            setup_requires=[  # can be removed whenever we can drop pip 9 support
+                'setuptools_scm',  # so that version will work
+                'setuptools_scm_git_archive',  # enable version from github tarballs
+            ],
+            ext_modules=[extmodule],
+            use_scm_version=True,
+            cmdclass={"build_ext": build_ext},  # type: ignore
+        )
