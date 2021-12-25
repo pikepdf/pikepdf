53d6b8f5b997e30f3a72230ee5ce053a250bc0d6
James R. Barlow <james@purplerock.ca>
2021-12-25 00:59:29 -0800

    xml: stricter typing

--- src/pikepdf/_xml.py
+++ src/pikepdf/_xml.py
@@ -8,11 +8,12 @@
 from typing import IO, Any, AnyStr, Union
 
 from lxml.etree import XMLParser as _UnsafeXMLParser
+from lxml.etree import _ElementTree
 from lxml.etree import parse as _parse
 
 
 class _XMLParser(_UnsafeXMLParser):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any):
         # Prevent XXE attacks
         # https://rules.sonarsource.com/python/type/Vulnerability/RSPEC-2755
         kwargs['resolve_entities'] = False
@@ -20,7 +21,7 @@ class _XMLParser(_UnsafeXMLParser):
         super().__init__(*args, **kwargs)
 
 
-def parse_xml(source: Union[AnyStr, IO[Any]], recover: bool = False):
+def parse_xml(source: Union[AnyStr, IO[Any]], recover: bool = False) -> _ElementTree:
     """Wrapper around lxml's parse to provide protection against XXE attacks."""
 
     parser = _XMLParser(recover=recover, remove_pis=False)
