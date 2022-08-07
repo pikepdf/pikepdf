a3ff0cc5c4ed0496c7bbabf171e9dde50abc596c
James R. Barlow <james@purplerock.ca>
2022-08-07 01:08:48 -0700

    More remove PointerHolder

--- src/qpdf/annotation.cpp
+++ src/qpdf/annotation.cpp
@@ -5,7 +5,6 @@
 #include <qpdf/Types.h>
 #include <qpdf/DLL.h>
 #include <qpdf/QPDFExc.hh>
-#include <qpdf/PointerHolder.hh>
 #include <qpdf/QPDFAnnotationObjectHelper.hh>
 
 #include <pybind11/pybind11.h>
