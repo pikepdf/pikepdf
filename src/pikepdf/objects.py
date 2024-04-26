dda534f8b532c6aa0102a84c8edc83ef38f68930
Anthony Margerand <anthony@happywait.com>
2024-04-26 09:18:33 +0200

    Remove

--- src/pikepdf/objects.py
+++ src/pikepdf/objects.py
@@ -98,8 +98,8 @@ class Name(Object, metaclass=_NameObjectMeta):
         """Construct a PDF Name."""
         # QPDF_Name::unparse ensures that names are always saved in a UTF-8
         # compatible way, so we only need to guard the input.
-        # if isinstance(name, bytes):
-        #     raise TypeError("Name should be str")
+        if isinstance(name, bytes):
+            raise TypeError("Name should be str")
         if isinstance(name, Name):
             return name  # Names are immutable so we can return a reference
         return _core._new_name(name)
