6d7a02c73443da3333383cc6de104e24ef27e390
bcapuano <bcapuano@asu.edu>
2024-09-02 14:38:22 -0700

    Iteration testing build.sh

--- fuzzing/build.sh
+++ fuzzing/build.sh
@@ -1,8 +1,8 @@
-cd "$SRC"/dateparser
-pip3 install .
+cd "$SRC"/pikepdf
+CC="`which clang`" CFLAGS="-fsanitize=address,fuzzer-no-link" CXX="`which clang++`" CXXFLAGS="-fsanitize=address,fuzzer-no-link" LDSHARED="`which clang++` -shared" pip3 install .
 
 # Build fuzzers in $OUT
 for fuzzer in $(find fuzzing -name '*_fuzzer.py');do
   compile_python_fuzzer "$fuzzer"
 done
-zip -q $OUT/dateparser_fuzzer_seed_corpus.zip $SRC/corpus/*
+zip -q $OUT/pikepdf_fuzzer_seed_corpus.zip $SRC/pikepdf/fuzzing/corpus/*
