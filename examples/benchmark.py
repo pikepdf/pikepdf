861a2fb580a27def32eead4ea2463d35f1786911
James R. Barlow <james@purplerock.ca>
2025-04-05 00:34:03 -0700

    Add a quick benchmark

---
+++ examples/benchmark.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import time
+from tempfile import NamedTemporaryFile
+
+import pikepdf
+
+
+def main():
+    start = time.monotonic()
+    with pikepdf.open("tests/resources/private/PDF-RM1.7.pdf") as pdf:
+        for p in pdf.pages:
+            p.Contents.read_bytes()
+        for o in pdf.objects:
+            if isinstance(o, (pikepdf.Stream, pikepdf.Dictionary, pikepdf.Array)):
+                o.unparse()
+        with NamedTemporaryFile(suffix=".pdf") as f:
+            pdf.save(
+                f,
+                object_stream_mode=pikepdf.ObjectStreamMode.generate,
+                normalize_content=True,
+                recompress_flate=True,
+            )
+    end = time.monotonic()
+    print(f"Time: {end - start}s")
+
+
+if __name__ == "__main__":
+    main()
