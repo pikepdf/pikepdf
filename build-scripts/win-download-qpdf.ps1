930e706832912f924344ff154f010c01365674c9
James R. Barlow <james@purplerock.ca>
2025-08-19 15:03:42 -0700

    Fix build selector

--- build-scripts/win-download-qpdf.ps1
+++ build-scripts/win-download-qpdf.ps1
@@ -4,7 +4,7 @@
 $version = $args[0]
 $platform = $args[1]
 
-if ($platform -eq "windows") {
+if ($platform -eq "win_amd64" -or $platform -eq "win_arm64") {
     $msvc = "msvc64"
 } else {
     throw "I don't recognize platform=$platform"
